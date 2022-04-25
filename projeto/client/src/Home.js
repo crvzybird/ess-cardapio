334c58969f7a1d9358881bb75104382a238aff05
Matheus Costa <matheuscbsilva402@gmail.com>
2022-04-25 00:35:41 -0300

    added the read request to home

--- projeto/client/src/Home.js
+++ projeto/client/src/Home.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
+import Axios from 'axios';
+
+import Add from "./Add";
 
 import Button from '@mui/material/Button';
 
 import logo from "./brigadeiro.png";
+
+import variables from './variables.json';
 
 const Home = () => {
 
     let navigate = useNavigate ();
 
     const [open, setOpen] = useState(false);
+    const [showItems, setShowItems] = useState();
+
+    useEffect(() => {
+        Axios.get(variables.URL + "all").then((response) => {
+          setShowItems(response.data);
+        }, [showItems]);
+    });
 
     return (
         <div className="container">
@@ -21,12 +33,27 @@ const Home = () => {
                 </div>
 
                 <div className="navigation">
+                    <Add
+                        open={open}
+                        setOpen={setOpen}                        
+                        showItems={showItems} 
+                        setShowItems={setShowItems}
+                    />
                     <Button variant="contained" onClick={() => {navigate("/");}}>Home</Button>
                     <Button variant="contained" onClick={() => setOpen(true)}>Add</Button>
                 </div>
             
             </div>
 
+            <div className="MenuItems">
+                {typeof showItems !== "undefined" && showItems.map((item) => {
+                    return(
+                        <h2> I'm a test! </h2>
+                    );
+                })}
+            </div>
+
+  
         </div>
     );
 
