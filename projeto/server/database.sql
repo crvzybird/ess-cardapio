ff1e23535e85bedb6da6e0339e23a7ba272b4b29
costa402 <43799754+costa402@users.noreply.github.com>
2022-05-04 10:20:16 -0300

    updated "database.sql" to fit requested changes

--- projeto/server/database.sql
+++ projeto/server/database.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE cardapio;
 
 CREATE TABLE item(
-    item_id SERIAL PRIMARY KEY,
-    item_name VARCHAR(50),
-    item_price VARCHAR(5),
-    item_description VARCHAR(200)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    price VARCHAR(5),
+    description VARCHAR(200)
 ); 
