1d4ef9899e43f2aabfe634a2d6d19846b01217d4
crvzybird <rtulyo@hotmail.com>
2022-05-05 03:26:34 -0300

    Add condition to isMenu for EditBox pop-up on edit page

--- projeto/client/src/Item.js
+++ projeto/client/src/Item.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
+import EditBox from "./EditBox";
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Edit from '@mui/icons-material/Edit';
 import Typography from '@mui/material/Typography';
 
-const Item = ({isMenu, id, description, name, price, showItems, setShowItems,  }) => {
-    const ShowEditButton = () => {
-        if(!isMenu){
-            return(
-                <CardActions>
-                    <Button size="small" variant="outlined">Edit</Button>
-                </CardActions>
-            );
-        }
-    }
+const Item = ({isMenu, id, description, name, price, showItems, setShowItems}) => {
+    
+    const [open, setOpen] = useState(false);
 
     return ( 
         <div className="Item">
+             <EditBox 
+                id={id}
+                description={description}
+                name={name}
+                price={price}
+                open={open}
+                setOpen={setOpen}
+                showItems={showItems} 
+                setShowItems={setShowItems}
+            />
             <Card sx={{ maxWidth: 345 }} key={id}>
                 <CardMedia
                     component="img"
@@ -40,7 +45,12 @@ const Item = ({isMenu, id, description, name, price, showItems, setShowItems,  }
                         {description}
                     </Typography>
                 </CardContent>
-                < ShowEditButton />
+                {!isMenu 
+                    ? <CardActions>
+                        <Button size="small" variant="outlined" startIcon={<Edit />} onClick={() => setOpen(true)}>Edit</Button>
+                    </CardActions>
+                    : null
+                }
             </Card>
         </div>
 
