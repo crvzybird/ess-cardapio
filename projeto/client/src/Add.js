469db5bab493ef45d317268b96ced35678123b1c
Letícia <39385359+leehmaciel@users.noreply.github.com>
2022-05-05 23:24:04 -0300

    Clear inputs after adding an item (#37)

--- projeto/client/src/Add.js
+++ projeto/client/src/Add.js
@@ -14,7 +14,7 @@ import variables from './variables.json';
 
 const Add = ({open, setOpen, showItems, setShowItems}) => {
 
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, reset} = useForm();
 
     const handleClose = () => {
         setOpen(false);
@@ -28,6 +28,7 @@ const Add = ({open, setOpen, showItems, setShowItems}) => {
         }).then((response) => {
                 console.log(response);
                 handleClose();
+                reset();
             });
     }
 
