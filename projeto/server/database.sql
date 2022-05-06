b4af128ac676ba970906a519134c0a1673dc3a54
Raphael Tulyo <rtulyo@hotmail.com>
2022-05-06 11:45:57 -0300

    Revert "Improviments and code refactor"

--- projeto/server/database.sql
+++ projeto/server/database.sql
@@ -1,22 +1,8 @@
-CREATE TABLE item (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `price` INT NOT NULL,
-  `description` VARCHAR(200) NULL,
-  PRIMARY KEY (`id`));
+CREATE DATABASE cardapio;
 
-CREATE TABLE category (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
-
-CREATE TABLE category_items(
-    category_id INT(15) NOT NULL,
-    item_id INT(15) NOT NULL,
-    FOREIGN KEY(category_id)
-    REFERENCES category(id)
-    ON DELETE CASCADE,
-    FOREIGN KEY(item_id)
-    REFERENCES item(id)
-    ON DELETE CASCADE,
-    UNIQUE(item_id, category_id));
+CREATE TABLE item(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    price VARCHAR(5),
+    description VARCHAR(200)
+); 
