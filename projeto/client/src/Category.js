c21f14321b0d60b1b412a054b2d8ed539b753ded
mpcabral <matheus.cabral@tild.com.br>
2022-05-06 05:14:12 -0300

    criaçao da feature de adicionar/remover item a uma categoria

--- projeto/client/src/Category.js
+++ projeto/client/src/Category.js
@@ -22,6 +22,7 @@ const Add = ({open, setOpen, showItems, setShowItems}) => {
     const [toDelete, setToDelete] = React.useState('');
     const [category, setCategory] = React.useState();
     
+    
     React.useEffect(() => {
         Axios.get(variables.URL + "category").then((response) => {
            setCategory(response.data);
@@ -30,6 +31,18 @@ const Add = ({open, setOpen, showItems, setShowItems}) => {
 
     const handleChange = (event) => {
         setToDelete(event.target.value);
+    };
+
+    const [addCategory1, setAddCategory1] = React.useState();
+
+    const handleChangeAddCategory1 = (event) => {
+        setAddCategory1(event.target.value);
+    };
+
+    const [addCategory2, setAddCategory2] = React.useState();
+    
+    const handleChangeAddCategory2 = (event) => {
+        setAddCategory2(event.target.value);
     };
 
     const handleClose = () => {
@@ -46,6 +59,38 @@ const Add = ({open, setOpen, showItems, setShowItems}) => {
                 reset();
             });
     }
+
+    const handleDeleteIC = (values) => {
+        console.log("deleting")
+        console.log(addCategory1)
+        console.log(addCategory2)
+        Axios.delete(variables.URL + "itemToCategory", {
+            data: {
+                itemid: addCategory2,
+                categoryid: addCategory1 
+            }
+        }).then((response) => {
+                console.log(response);
+                handleClose();
+                reset();
+            });
+    }
+
+    const onAddItemCategory = (values) => {
+        
+    }
+
+    const handleAddIC = (values) => {
+        Axios.post(variables.URL + "itemToCategory", {
+            itemid: addCategory2,
+            categoryid: addCategory1 
+        }).then((response) => {
+                console.log(response);
+                handleClose();
+                reset();
+            });
+    }
+
     const onDeleteCategory = (values) => {
         console.log("deleting")
         console.log(values)
@@ -62,7 +107,7 @@ const Add = ({open, setOpen, showItems, setShowItems}) => {
 
         <div>
             <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Add</DialogTitle>
+                <DialogTitle id="form-dialog-title">Create</DialogTitle>
                 <form onSubmit={handleSubmit(onAddCategory)}>
                     <DialogContent>
 
@@ -80,7 +125,7 @@ const Add = ({open, setOpen, showItems, setShowItems}) => {
                     </DialogContent>
                     
                     <DialogActions>
-                        <Button variant="contained" type="submit">Add</Button>
+                        <Button variant="contained" type="submit">Create</Button>
                     </DialogActions>
                     
                 </form>
@@ -109,7 +154,50 @@ const Add = ({open, setOpen, showItems, setShowItems}) => {
                     <DialogActions>
                         <Button variant="contained" type="submit">Delete</Button>
                     </DialogActions>
+                </form>
+
+                <DialogTitle id="form-dialog-title">Item to category</DialogTitle>
+                <form onSubmit={handleSubmit(onAddItemCategory)}>
+                    <DialogContent>
+                    <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                    <Select
+                    required
+                    fullWidth
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={addCategory1}
+                    label="category"
+                    onChange={handleChangeAddCategory1}
+                    >
                     
+                    {typeof category !== "undefined" && category.map((item) => {
+                        return(<MenuItem value={item.id}>{item.name}</MenuItem>)
+                    })}
+
+                    </Select>
+                    <InputLabel id="demo-simple-select-label">Item</InputLabel>
+                    <Select
+                    required
+                    fullWidth
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={addCategory2}
+                    label="item"
+                    onChange={handleChangeAddCategory2}
+                    >
+                    
+                    {typeof showItems !== "undefined" && Array.isArray(showItems) && showItems.map((item) => {
+                        return(<MenuItem value={item.id}>{item.name}</MenuItem>)
+                    })}
+
+                    </Select>
+
+                    </DialogContent>
+
+                    <DialogActions>
+                    <Button variant="contained" type="submit" onClick={handleDeleteIC} color="error">Remove</Button>
+                    <Button variant="contained" type="submit" onClick={handleAddIC}>Add</Button>
+                    </DialogActions>
                 </form>
                 
 
